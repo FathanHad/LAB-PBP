9c9622e31ed07fe7bb83eb84847e522d04b54c86
fathanhadyan <fathanhadyan777@gmail.com>
2022-09-21 23:39:57 +0700

    commit lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,6 +3,9 @@ from wishlist.views import JSON_by_id, show_wishlist
 from wishlist.views import XML_wishlist
 from wishlist.views import JSON_wishlist
 from wishlist.views import JSON_by_id, XML_by_id
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -12,4 +15,7 @@ urlpatterns = [
     path('json/', JSON_wishlist, name='JSON_wishlist'),
     path('json/<int:id>', JSON_by_id, name='JSON_by_id'),
     path('xml/<int:id>', XML_by_id, name='XML_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
